e6bbea9203c3c499f93d3f65d395b956fa4f9696
Mikael Josias <mikaeljosias2002@gmail.com>
2023-04-09 12:08:21 -0300

    feat: insert product route

--- src/controllers/products-controllers.ts
+++ src/controllers/products-controllers.ts
@@ -11,6 +11,17 @@ async function getAll (req: Request, res: Response, next: NextFunction) {
     }
 }
 
+async function insertNew (req: Request, res: Response, next: NextFunction) {
+    try {
+        const product = await productsServices.insertNew(req.body);
+        res.status(201).send(product);
+    } catch (error) {
+        next(error);
+        res.sendStatus(500);
+    }
+}
+
 export default {
-    getAll
+    getAll,
+    insertNew
 }
--- src/routes/products-router.ts
+++ src/routes/products-router.ts
@@ -1,9 +1,13 @@
 import productsControllers from "../controllers/products-controllers";
+import { validateBody } from "../middlewares/validation-middleware";
+import { productsSchema } from "../schemas/products-schema";
 import { Router } from "express";
+
 
 
 const router = Router();
 
 router.get("/all", productsControllers.getAll);
+router.post("/new", validateBody(productsSchema), productsControllers.insertNew);
 
 export default router;
