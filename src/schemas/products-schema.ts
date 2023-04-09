a4a6d29f582e687ca18004bf4307f0fd54da610d
Mikael Josias <mikaeljosias2002@gmail.com>
2023-04-09 12:28:15 -0300

    feat: delete product body schema

--- src/schemas/products-schema.ts
+++ src/schemas/products-schema.ts
@@ -5,3 +5,7 @@ export const productsSchema = Joi.object({
     price: Joi.number().positive().integer().required(),
     stock: Joi.number().positive().integer().required()
 });
+
+export const deleteProductSchema = Joi.object({
+    id: Joi.number().integer().positive().required()
+});
